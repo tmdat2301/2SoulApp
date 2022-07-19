5c3635973f8d51e05a827cc12758cf250a303041
Tran Dat <dattranmanh36@gmail.com>
2022-07-19 17:31:56 +0700

    firstcommit

---
+++ app/src/main/java/com/quintus/labs/datingapp/customfonts/EditText_Roboto_Meidum.java
@@ -0,0 +1,36 @@
+package com.quintus.labs.datingapp.customfonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+
+
+
+
+public class EditText_Roboto_Meidum extends androidx.appcompat.widget.AppCompatEditText {
+
+    public EditText_Roboto_Meidum(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public EditText_Roboto_Meidum(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public EditText_Roboto_Meidum(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/Roboto-Medium.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
